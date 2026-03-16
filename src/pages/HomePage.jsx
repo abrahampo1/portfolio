365d438ff128b5af46adf44aa87e13d9b132d4a7
Abraham Leiro <abraham@tubuencamino.com>
2026-03-16 17:41:40 +0100

    Fetch projects dynamically from utils API instead of hardcoding them

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import GitHubStats from '../components/GithubStats';
 import { Project } from '../components/Project';
 
 export default function HomePage() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await axios.get('https://utils.leiro.dev/api/projects');
+        setProjects(response.data);
+      } catch (error) {
+        console.error('Error fetching projects:', error);
+      }
+      setLoading(false);
+    };
+
+    fetchProjects();
+  }, []);
+
   return (
     <>
       <div className="mt-2">
@@ -10,15 +29,21 @@ export default function HomePage() {
       <div className="mt-16">
         <p className="text-3xl tinos-regular">Portfolio</p>
         <div className="flex gap-2 flex-wrap justify-center md:justify-start mt-2">
-          <Project projectName='Asoft' projectCharge='Full Stack Developer' projectUrlLabel='asoft.es' projectImage='https://www.asoft.es/asoft-logo.png' projectUrl='https://asoft.es' />
-          <Project projectName='BloxyCorp' projectCharge='Full Stack Developer' projectUrlLabel='bloxycorp.com' projectUrl='https://bloxycorp.com' projectImage='https://www.bloxycorp.com/logo.svg' darkMode />
-          <Project projectName='Tubuencamino' projectCharge='Full Stack Developer' projectUrlLabel='tubuencamino.com' projectUrl='https://tubuencamino.com' projectImage='https://tubuencamino.com/imagenes/logotipo/logo-tbc.compressed.png' />
-          <Project projectName='Dr.Whisk3rs' projectCharge='Full Stack Developer' projectUrlLabel='drwhisk3rs.com' projectUrl='https://drwhisk3rs.com' projectImage='https://i.imgur.com/R8Wo52T.png' />
-          <Project projectName='Pumm' projectCharge='Designer' projectUrl='https://pumm.io' projectUrlLabel='pumm.io' projectImage='https://pumm.io/assets/pummlogo.a0d7a614.png' />
-          <Project projectName='Roam' projectCharge='Full Stack Developer' projectUrl='https://abrahampo1.github.io/roam/' projectUrlLabel='github.io/roam' projectImage='https://github.com/abrahampo1/roam/blob/master/src/image/logo.png?raw=true' />
-          <Project projectName='TBI' projectUrlLabel='tbi-software.com' projectCharge='Full Stack Developer' projectImage='https://tbi-software.com/tbi-logo.png' projectUrl='https://tbi-software.com' />
-          <Project projectName='Cannagest' projectCharge='Full Stack Developer' projectUrlLabel='cannagest.com' projectImage='https://www.asoft.es/asoft-logo.png' projectUrl='https://cannagest.com' />
-          <Project projectName='CryptoGest' projectCharge='Full Stack Developer' projectUrlLabel='cryptogest.app' projectImage='https://cryptogest.app/images/logo.png' projectUrl='https://cryptogest.app' />
+          {loading ? (
+            <p className="tinos-regular-italic">Loading projects...</p>
+          ) : (
+            projects.map((project) => (
+              <Project
+                key={project.id}
+                projectName={project.name}
+                projectCharge={project.charge}
+                projectUrlLabel={project.url_label}
+                projectImage={project.image}
+                projectUrl={project.url}
+                darkMode={project.dark_mode}
+              />
+            ))
+          )}
         </div>
       </div>
       <GitHubStats />
